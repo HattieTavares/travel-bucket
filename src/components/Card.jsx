5f82865730a4f98f64895483774931bfa1bad106
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-10-22 16:14:22 -0400

    data and style basics

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <div className="imgSection">
+                <img src={props.each.imageUrl} className="image"/>
+            </div>
+            <div className="infoSection">
+                <div className="locSection">
+                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" className="marker"><path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path></svg>
+                    <span className="country">{props.each.location}</span>
+                    <a href="{props.each.googleMapsUrl}">View on Google Maps</a>
+                </div>
+                <span className="title">{props.each.title}</span>
+                <p>{props.each.description}</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/data.jsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        title: "Monserrate Palace",
+        location: "PORTUGAL",
+        googleMapsUrl: "https://goo.gl/maps/uUfM9iogB4xSqhMc6",
+        description: "There is no more unique building in the Sintra region than the Palacio de Monserrate. The villa is of a whimsical Arabian design, surrounded by classical English gardens, and set in the jagged hills of the Serra de Sintra. The tri-towered Palacio de Monserrate is the most visually striking building of Sintra, and was inspired by Islamic architecture and the ideal of symmetry.",
+        imageUrl: "https://source.unsplash.com/iqR0R7mzgpw"
+    },
+    {
+        id: 2,
+        title: "Atami Plum Garden",
+        location: "JAPAN",
+        googleMapsUrl: "https://goo.gl/maps/fpnRKRgr6BhdvjDm8",
+        description: "The Atami Plum Garden in Shizuoka is home to some of Japan's earliest blooming plum tree varieties. The annual plum festival held here showcases close to 500 plum trees in 59 different varieties, and along with the early blooming trees there are mid-blooming and late-blooming types. This ensures that whenever you visit during the festival dates, there will be something beautiful to enjoy!",
+        imageUrl: "https://source.unsplash.com/gG7LRXOfvDo"
+    },
+    {
+        id: 3,
+        title: "Sotoportego del Magazen",
+        location: "ITALY",
+        googleMapsUrl: "https://goo.gl/maps/unKVdMppUiSVCzfT6",
+        description: "Venice Italy is an Italian city located in the northeast of the State. Venice, Venezia in Italian, is best known for its beauty, canals, architecture, and art. This small city founded in the middle of a lagoon has been one the most powerful European States for several centuries, giving birth to a culture and lifestyle that are incredibly unique. Venice has been for centuries the favourite city of artists and inventors.",
+        imageUrl: "https://source.unsplash.com/Uu5aXBI1oLk"
+    }
+]
