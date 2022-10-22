578591d75a7a2e663474255ff20bb33a236890a1
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-10-22 17:53:42 -0400

    update Readme and fix map links

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-Travel Bucket List
+# Travel Bucket
+
+A simple React app featuring locations on my travel bucket list.
+
+**Link to project:** https://travelbucket.netlify.app/
+
+https://user-images.githubusercontent.com/82660400/194183688-b15ec8a2-e162-400d-9694-886e9a39cf13.mp4
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, React, Vite
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default function Card(props) {
                 <div className="locSection">
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" className="marker"><path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path></svg>
                     <span className="country">{props.each.location}</span>
-                    <a href="{props.each.googleMapsUrl}">View on Google Maps</a>
+                    <a href={props.each.googleMapsUrl}>View on Google Maps</a>
                 </div>
                 <span className="title">{props.each.title}</span>
                 <p>{props.each.description}</p>
